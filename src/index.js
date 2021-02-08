d2267c0cc791bd0e6402d70a2cd1c0a35175be91
Chichmarin17 <pits1917burg@gmail.com>
2021-02-08 19:15:22 +0300

    fix: return 0 if parametr is not defined

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 
 exports.min = function min (array) {
+  if (typeof(array) == "undefined") return 0;
   if (array.length == 0) return 0;
   return Math.min(...array);
 }
 
 exports.max = function max (array) {
+  if (typeof(array) == "undefined") return 0;
   if (array.length == 0) return 0;
   return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
+  if (typeof(array) == "undefined") return 0;
   if (array.length == 0) return 0;
   return array.reduce((a, b) => a + b) / array.length;
 }
